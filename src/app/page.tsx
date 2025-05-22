10a4155d8bba4e5407fa9bb845145a9ab28cd9a1
Anthony Ng <newyork.anthonyng@gmail.com>
2025-05-22 09:06:53 -0400

    wip: add bar charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import DesireWallContainer from "@/app/components/DesireWall";
+import Progress from "@/app/components/Progress";
 
 export default function Home() {
   return (
     <div>
       <DesireWallContainer />
+      <Progress />
     </div>
   );
 }
